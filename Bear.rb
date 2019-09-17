d612510d52c23cc8a4da94e6b1672c6938013ad6
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-17 20:52:21 +0100

    Bear class created and name tested

---
+++ Bear.rb
@@ -0,0 +1,12 @@
+class Bear
+
+attr_reader :name, :type
+
+  def initialize(name, type)
+    @name = name
+    @type = type
+    @stomach_contents = []
+  end
+
+
+end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -1,10 +1,19 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../Bear.rb')
+require_relative('../River.rb')
 
 class BearTest < MiniTest::Test
 
+  def setup
 
+    @bear_1 = Bear.new("Bruno", "Grizzly")
+
+  end
+
+  def test_bear_has_name
+    assert_equal("Bruno", @bear_1.name)
+  end
 
 
 end
